f2a26b7e0d455dcbef413e00f4be8e588201d249
ollyshit <46550165+ollyshit@users.noreply.github.com>
2019-01-10 10:47:41 +0700

    Add files via upload

---
+++ nyunthicc.php
@@ -0,0 +1,12 @@
+ <?php
+ echo "SUNTIK REFFERAL SI PUTRI";
+ echo "\n";
+ echo " Source : Olly";
+ echo "\n";
+ echo " Code Refferal : 5FU6CK";
+ echo "\n";
+for ($x = 500; $x <= 600; $x++) {
+    echo "\n Enak Susunya $x";
+    
+}
+?> 
